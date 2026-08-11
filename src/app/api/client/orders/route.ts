10fc46efdda331e477edc98167f9391522b0dd41
unknown <20mm313@esisalama.org>
2026-08-11 07:51:20 +0200

    fix: cast newOrder._id on table.currentOrderId

--- src/app/api/client/orders/route.ts
+++ src/app/api/client/orders/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: NextRequest) {
     });
 
     // Update table with currentOrderId and status occupied
-    table.currentOrderId = newOrder._id;
+    table.currentOrderId = newOrder._id as any;
     table.status = "occupied";
     await table.save();
 
